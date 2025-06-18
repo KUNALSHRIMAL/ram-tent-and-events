397b324f8b93609ff97a3a0797ec586b6a3979b5
Kunal Shrimal <2019pietcskunal86@poornima.org>
2025-06-19 03:42:06 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/ram-tent-and-events/', // 👈 VERY IMPORTANT
-})
